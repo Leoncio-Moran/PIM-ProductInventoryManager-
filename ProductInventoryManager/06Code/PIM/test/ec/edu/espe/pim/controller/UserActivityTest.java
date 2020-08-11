1ce1f64bd99420ae33f34e06631a1706690afb90
Johao01 <jamorales26@espe.edu.ec>
2020-08-11 08:05:43 -0500

    Code, unit test & calssdiagram to PIM System

---
+++ ProductInventoryManager/06Code/PIM/test/ec/edu/espe/pim/controller/UserActivityTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.pim.controller;
+
+import ec.edu.espe.pim.model.Users;
+import java.util.ArrayList;
+import org.junit.Test;
+
+/**
+ *
+ * @author lesly
+ */
+public class UserActivityTest {
+    
+    public UserActivityTest() {
+    }
+
+    @Test
+    public void testAddUser_String_String() {
+        UserActivity userActivity = new UserActivity();
+        String person = "Edison";
+        String pass = "POO";
+        userActivity.addUser(person, pass);
+
+    }
+
+    /**
+     * Test of readUsers method, of class UserActivity.
+     */
+    @Test
+    public void testReadUsers() {
+        UserActivity userActivity = new UserActivity();
+        ArrayList<Users> expResult = null;
+        ArrayList<Users> result = userActivity.readUsers();
+
+    }
+}
